210ee9844771f8ab1663c36c8d4295242823ca67
Lavanya Raju <lavanyaraju19@gmail.com>
2026-03-08 15:46:42 +0530

    Fix sitemap.js to return proper XML for Google indexing

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,13 +1,38 @@
-﻿export const revalidate = 1;
+export const revalidate = 1
 
 import { blogs } from '../data/blogs'
 
 const SITE_URL = 'https://acadvizen.com'
 
+/**
+ * Next.js metadata sitemap route.
+ * Next automatically converts this to XML and serves /sitemap.xml
+ * with Content-Type: application/xml.
+ *
+ * @returns {import('next').MetadataRoute.Sitemap}
+ */
 export default function sitemap() {
   const now = new Date()
 
-  const staticRoutes = ['/', '/about', '/blog', '/courses', '/contact']
+  const staticRoutes = [
+    '/',
+    '/about',
+    '/contact',
+    '/blog',
+    '/courses',
+    '/tools',
+    '/placement',
+    '/hire-from-us',
+    '/privacy-policy',
+    '/terms-of-service',
+    '/digital-marketing-course-in-bangalore',
+    '/digital-marketing-course-in-jayanagar',
+    '/seo-course-in-bangalore',
+    '/social-media-marketing-course-in-bangalore',
+    '/google-ads-course-in-bangalore',
+    '/ai-digital-marketing-course',
+    '/digital-marketing-internship-in-bangalore',
+  ]
 
   const staticEntries = staticRoutes.map((route) => ({
     url: `${SITE_URL}${route}`,
@@ -27,4 +52,3 @@ export default function sitemap() {
 
   return [...staticEntries, ...blogEntries]
 }
-
